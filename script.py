2e187601cf532f27affd39a9e28b623149513b15
ycat36 <74068072+IsaacHatton@users.noreply.github.com>
2023-12-16 13:04:10 +0000

    change to make it function on battery

--- script.py
+++ script.py
@@ -10,11 +10,13 @@ def is_admin():
         return False
 
 def disable_button():
+    os.system('powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0')
     os.system('powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0')
     os.system('powercfg -SetActive SCHEME_CURRENT')
 
 def enable_button():
-    os.system('powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 1')
+    os.system('powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 1')
+    os.system('powercfg -setacvalueindex  SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 1')
     os.system('powercfg -SetActive SCHEME_CURRENT')
 
 if is_admin():
